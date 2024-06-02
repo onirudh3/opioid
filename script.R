5061d50efec9f77fc7cb267c531f4c5bf52c8ec5
Anirudh Ravishankar <onirudh3@gmail.com>
2024-06-02 08:30:02 +0530

    updates june 2

--- script.R
+++ script.R
@@ -15,25 +15,23 @@ df <- read.csv("Data/VSRR_Provisional_Drug_Overdose_Death_Counts_20240528.csv")
 # We need only overdose deaths
 df <- df %>% subset(Indicator == "Number of Drug Overdose Deaths")
 
-# Merging year and month columns
-df <- df %>%
-  mutate(YearMonth = as.integer(paste0(Year, sprintf("%02d", match(Month, month.name)))),
-         .after = Month)
-
-# Rename Data.Value to Deaths
-df <- df %>% rename("Deaths" = "Data.Value")
-
-# Select necessary columns
-df <- subset(df, select = c(State.Name, YearMonth, Deaths))
+# 12 month ending period in December is the value for the year
+df <- df %>% subset(Month == "December")
 
 # Remove rows for United States and New York City which are not states
 df <- subset(df, !(State.Name %in% c("United States", "New York City")))
 
-# Assign ID to each state
-df <- df %>% group_by(State.Name) %>% mutate(State_ID = cur_group_id())
+# Select necessary columns
+df <- subset(df, select = c(State.Name, Year, Data.Value))
 
-# Arrange YearMonth by State
-df <- df %>% group_by(State.Name) %>% arrange(State.Name, YearMonth)
+# Rename Data.Value to Deaths
+df <- df %>% rename("Deaths" = "Data.Value")
+
+# Assign ID to each state
+df <- df %>% 
+  group_by(State.Name) %>% 
+  mutate(State_ID = cur_group_id()) %>% 
+  arrange(State_ID)
 
 
 # Prescription Policy Data ------------------------------------------------
